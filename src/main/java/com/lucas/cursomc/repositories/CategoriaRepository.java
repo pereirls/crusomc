8bd8d57199e4ed62a23e76975a986ecb81608403
pereirls <lucas_sp100@hotmail.com>
2019-06-22 15:24:18 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/lucas/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.lucas.cursomc.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucas.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+}
