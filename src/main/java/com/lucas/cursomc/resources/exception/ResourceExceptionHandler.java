be977b1a389c9bb6032bf81678adb906bcc5b76f
pereirls <lucas_sp100@hotmail.com>
2019-07-03 09:26:07 -0300

    Personalizando o retorno da aplicação

--- src/main/java/com/lucas/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import com.lucas.cursomc.services.exception.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +27,17 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
 		StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandartError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validacao", System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
