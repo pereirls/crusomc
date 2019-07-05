29f1c82f9f7d212f796c2ca35fa491f81c576436
pereirls <lucas_sp100@hotmail.com>
2019-07-05 17:04:22 -0300

    Ajustando deleção cliente

--- src/main/java/com/lucas/cursomc/domain/Cliente.java
+++ src/main/java/com/lucas/cursomc/domain/Cliente.java
@@ -6,16 +6,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lucas.cursomc.domain.enuns.TipoCliente;
+import org.hibernate.annotations.Cascade;
 
 @Entity
 public class Cliente implements Serializable {
@@ -29,7 +24,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
