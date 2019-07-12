eacf6ed3c6eb0dd15476c2aef23978e5708346b3
pereirls <lucas_sp100@hotmail.com>
2019-07-12 11:24:33 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/com/lucas/cursomc/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.lucas.cursomc.config;
+
+import com.lucas.cursomc.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDataBase() throws Exception {
+        if(!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instantiateDastaBase();
+        return true;
+    }
+}
