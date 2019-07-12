7b97edc7e149d37eaea476daf51c4b38ad09832c
pereirls <lucas_sp100@hotmail.com>
2019-07-12 14:25:06 -0300

    email HTML

--- src/main/java/com/lucas/cursomc/services/EmailService.java
+++ src/main/java/com/lucas/cursomc/services/EmailService.java
@@ -3,11 +3,16 @@ package com.lucas.cursomc.services;
 import com.lucas.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
 
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 
 }
